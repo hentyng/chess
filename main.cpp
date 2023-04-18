9989a1fd3dd91ee0e55101f8947574bc05b61496
hentyng <130176425+hentyng@users.noreply.github.com>
2023-04-17 20:48:38 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,14 +11,14 @@ int main(){
 	string s;
 	bool newgame = true;
 	cout << "   _____ _    _ ______  _____ _____ \n  / ____| |  | |  ____|/ ____/ ____| \n | |    | |__| | |__  | (___| (___  \n | |    |  __  |  __|  \\___  \\___ \\ \n | |____| |  | | |____ ____) |___) | \n  \\_____|_|  |_|______|_____/_____/ \n" << endl;
-	cout << "A game by hen"<<endl;
+	cout << "Made by henry "<<endl;
 	cout << "Enter any key to continue" << endl;
-	//cin >> s;
+	cin >> s;
 
 	while(newgame){
 		b.setBoard();
 		while (b.playGame());
-		cout << "Do you want to play again? (y for yes, anything else for no) ";
+		cout << "Play Again? Press 'y' for yes, anything else for no";
 		cin >> s;
 		if (s != "y"){
 			newgame = false;
